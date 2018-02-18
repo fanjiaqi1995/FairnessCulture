c8a87d50a83e2c83eab2e840b84db857e8b6c99e
wesker <897315779@qq.com>
2018-02-18 21:16:35 +0800

    初始项目推送

---
+++ html/test.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+header("Content-Type:text/html;charset=utf-8");
+/**
+ * Created by PhpStorm.
+ * User: fanji
+ * Date: 2018/2/18
+ * Time: 21:16
+ */?>
+<!doctype html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
